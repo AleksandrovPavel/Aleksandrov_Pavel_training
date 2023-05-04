5e8e2ea38eafa07ceffb405475190336e46e8845
AleksandrovPavel <pvaleksandrov@gmail.com>
2023-05-04 17:48:11 +0500

    add Tasks/Task_05

--- Tasks/Task 05/src/main/java/jdbc/dao/UserDaoHibernateImpl.java
+++ Tasks/Task 05/src/main/java/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,9 +3,10 @@ package jdbc.dao;
 
 import jdbc.model.User;
 import jdbc.util.HibernateUtil;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -48,10 +49,13 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
 
-            session.beginTransaction();
+        try {
+
+            transaction = session.beginTransaction();
 
             User user = new User();
             user.setName(name);
@@ -59,10 +63,14 @@ public class UserDaoHibernateImpl implements UserDao {
             user.setAge(age);
 
             session.persist(user);
-            session.getTransaction().commit();
+            transaction.commit();
 
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
         }
 
 
@@ -71,57 +79,78 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
 
-            session.beginTransaction();
+        try {
 
-            User user = new User();
-            user.setId(id);
+            transaction = session.beginTransaction();
 
-            session.remove(user);
-            session.getTransaction().commit();
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.remove(user);
+            }
+
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
         }
-
-
     }
 
     @Override
     public List<User> getAllUsers() {
 
-        List<User> users;
+        List<User> users = null;
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
 
-            session.beginTransaction();
+        try {
+
+            transaction = session.beginTransaction();
 
             users = session.createQuery("from User", User.class).list();
 
-            session.getTransaction().commit();
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
         }
 
         return users;
     }
 
-    @Override
     public void cleanUsersTable() {
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        try {
 
             String sql = "DELETE FROM test.users";
 
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createNativeQuery(sql, User.class).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
 
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
         }
     }
 }
